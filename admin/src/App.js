4a8e2ce1e0e4f3119b1ff5ee90767726886ba0e1
Mauro Colucci <colucci.mauro@gmail.com>
2022-08-08 17:15:50 -0300

    barebones of admin page completed. mvp achieved

--- admin/src/App.js
+++ admin/src/App.js
@@ -3,6 +3,12 @@ import Topbar from "./components/topbar/Topbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './app.css'
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import NewProduct from "./pages/newProduct/NewProduct"
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
 
 function App() {
   return (
@@ -10,7 +16,15 @@ function App() {
       <Topbar/>
       <div className="container">
         <Sidebar/>
-        <Home/>
+        <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/users" element={<UserList/>}/>
+         <Route path="/user/:userId" element={<User/>}/>
+         <Route path="/newUser" element={<NewUser/>}/>
+         <Route path="/newProduct" element={<NewProduct/>}/>
+         <Route path="/products" element={<ProductList/>}/>
+         <Route path="/product/:productsId" element={<Product/>}/>
+        </Routes>
       </div>
     </Router>
   );
